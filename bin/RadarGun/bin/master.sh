9db13437d8214336d20184886d65b7304d413978
Fabio Perfetti <perfabio87@gmail.com>
2013-02-14 15:31:10 +0000

    benchmark_os.sh tested. Ready to use

--- bin/RadarGun/bin/master.sh
+++ bin/RadarGun/bin/master.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 ## Load includes
 if [ "x$RADARGUN_HOME" = "x" ]; then DIRNAME=`dirname $0`; RADARGUN_HOME=`cd $DIRNAME/..; pwd` ; fi; export RADARGUN_HOME
 . ${RADARGUN_HOME}/bin/includes.sh
